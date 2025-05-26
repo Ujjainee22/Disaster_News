1d03c7acde34f001e12a9b0ab07ecdd9db2fcb05
Ujjainee22 <ujjaineeushasi4321@gmail.com>
2025-05-27 01:43:12 +0530

    simple

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,3 +1,4 @@
 
 const {username,password}=process.env;
-export const connStr = "mongodb+srv://ttgpt25:"+password+"@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1";
+//export const connStr = "mongodb://localhost:27017/";
+export const connStr = "mongodb+srv://Ujj:"+password+"@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1";
