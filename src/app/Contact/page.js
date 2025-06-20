ceb77fbb7596071b9cb1549e6adf88145708cba8
Ujjainee22 <ujjaineeushasi4321@gmail.com>
2025-06-21 02:03:57 +0530

    Fixed casing issue in Contact folder

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "@/app/contact/contact.module.css"
+import styles from "@/app/Contact/contact.module.css"
 import ContactCard from "@/app/Contact/ContactCard"
 import Footer from '../components/footer'
 import Header from "@/app/components/header";
