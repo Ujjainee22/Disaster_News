e33cb26e177dcc5675af397f615b004d10e788fb
Ujjainee22 <ujjaineeushasi4321@gmail.com>
2025-06-20 22:03:30 +0530

    change for mongodb

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,12 @@
 
-const {username,password}=process.env;
+//const {username,password}=process.env;
 //export const connStr = "mongodb://localhost:27017/";
-export const connStr = "mongodb+srv://Ujj:"+password+"@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1";
+
+
+//export const connStr = "mongodb+srv://"+username+":"+password+"@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1";
+
+const username = process.env.MONGO_USER;
+const encodedPassword = encodeURIComponent(process.env.MONGO_PASS);
+export const connStr = `mongodb+srv://${process.env.MONGO_USER}:${encodeURIComponent(process.env.MONGO_PASS)}@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1`;
+
+
