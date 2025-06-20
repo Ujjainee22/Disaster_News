8d13ee4d5a79fda92c1e7c743ad483583072eaa1
Ujjainee22 <ujjaineeushasi4321@gmail.com>
2025-06-21 00:07:17 +0530

    00.07

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,9 @@
-
-//const {username,password}=process.env;
-//export const connStr = "mongodb://localhost:27017/";
-
-
-//export const connStr = "mongodb+srv://"+username+":"+password+"@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1";
-
 const username = process.env.MONGO_USER;
-const encodedPassword = encodeURIComponent(process.env.MONGO_PASS);
-export const connStr = `mongodb+srv://${process.env.MONGO_USER}:${encodeURIComponent(process.env.MONGO_PASS)}@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1`;
+const password = process.env.MONGO_PASS;
 
+//console.log("Username:", username);
+//console.log("Password:", password);
 
+export const connStr = `mongodb+srv://${username}:${password}@learntest1.rrvfqq5.mongodb.net/?retryWrites=true&w=majority&appName=learnTest1`;
+
+//console.log("Connection String:", connStr);
